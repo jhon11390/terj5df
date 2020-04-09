df9c509ca5d48684a19ab1b470d1703b851436af
jhon11390 <meljhon2@gmail.com>
2020-04-09 04:02:38 -0500

    se contruye funcionalidad para el formulario

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      persons: [],
+      nameinput: '',
+      lastnameinput: '',
+      number: 1
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.addPerson.bind(this)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" className="form-control" name="first-name" value={this.state.nameinput} onChange={this.updatePersonname.bind(this)}/>
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" className="form-control" name="last-name" value={this.state.lastnameinput} onChange={this.updatePersonlastname.bind(this)}/>
               </div>
 
               <div className="action">
@@ -30,13 +40,37 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.persons.map((person) => <tr key={person.id}><td>{person.name}</td><td>{person.lastname}</td></tr>)}
               </tbody>
             </table>
           </div>
         </div>
       </div>
     )
+  }
+
+  updatePersonname(event) {
+    this.setState({
+      nameinput: event.target.value,
+    })
+  }
+
+  updatePersonlastname(event) {
+    this.setState({
+      lastnameinput: event.target.value
+    })
+  }
+
+  addPerson(event) {
+    event.preventDefault();
+    if(this.state.nameinput.length > 0 && this.state.lastnameinput.length > 0){
+      this.setState({
+        persons: this.state.persons.concat({id: this.state.number, name: this.state.nameinput, lastname:this.state.lastnameinput}),
+        nameinput: '',
+        lastnameinput: '',
+        number: this.state.number + 1
+      })
+    }
   }
 }
 
